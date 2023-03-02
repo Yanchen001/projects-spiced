7bc9193b6100f26a3d896a1a2e6c74deb949d74b
Yan Chen <ychen90@outlook.com>
2023-03-02 11:38:34 +0100

    add dark mode

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const bodyElement = document.querySelector('[data-js="body"]')
+const toggleButton = document.querySelector ('[data-js="toggle-button"]')
+
+
+toggleButton.addEventListener("click", (e)=> {
+    bodyElement.classList.toggle("dark");
+})
+
