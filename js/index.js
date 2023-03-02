ed83117e69555eb2afd432f3e065c131a5a7d442
Yan Chen <ychen90@outlook.com>
2023-03-02 17:14:35 +0100

    commentout dark mode. add answer toggle

--- js/index.js
+++ js/index.js
@@ -1,8 +1,32 @@
+console.clear();
+
+/* Dark Mode */
+
 const bodyElement = document.querySelector('[data-js="body"]')
 const toggleButton = document.querySelector ('[data-js="toggle-button"]')
 
-
+/*
 toggleButton.addEventListener("click", (e)=> {
     bodyElement.classList.toggle("dark");
 })
+
+*/
+
+
+/* Show Answer*/
+
+const showAnswerButton = document.querySelector('[data-js="show_answer"]')
+const displayAnswer = document.querySelector('[data-js="answer"]')
+
+
+showAnswerButton.addEventListener("click", (e)=> {
+    displayAnswer.toggleAttribute("hidden");
+    if (displayAnswer.hidden){
+        showAnswerButton.textContent="Show Answer"
+    } else {
+        showAnswerButton.textContent="Hide Answer"
+    }
+
+})
+
 
